6e2fd32df794269ab442b03d89e6548ca3063b6c
Yousef <yahmed1996a@gmail.com>
2019-11-08 14:58:15 +0000

    SQL file errors fixed/formatted correctly

--- db/records_inventory.sql
+++ db/records_inventory.sql
@@ -4,12 +4,12 @@ DROP TABLE artists;
 
 
 CREATE TABLE artists(
-  id SERIAL4,
+  id SERIAL4 PRIMARY KEY,
   name VARCHAR(255)
 );
 
 CREATE TABLE record_labels(
-  id SERIAL4,
+  id SERIAL4 PRIMARY KEY,
   name VARCHAR(255),
   contact_info VARCHAR(255)
 );
@@ -21,7 +21,7 @@ CREATE TABLE records(
   genre VARCHAR(255),
   buy_cost INT4,
   sell_cost INT4,
-  artist_id INT4 REFERENCES FROM artists(id),
-  record_label_id INT4 REFERENCES FROM record_labels(id)
+  artist_id INT4 REFERENCES artists(id),
+  record_label_id INT4 REFERENCES record_labels(id),
   quantity INT4
 );
