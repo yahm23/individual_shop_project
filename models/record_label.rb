b62f068409cbc566f9702b119ab2d4b80e730e36
Yousef <yahmed1996a@gmail.com>
2019-11-08 15:32:46 +0000

    RecordLabel intialize coded

--- models/record_label.rb
+++ models/record_label.rb
@@ -1,1 +1,18 @@
 require_relative( '../db/sql_runner' )
+
+class RecordLabel
+  attr_reader :id
+  attr_accessor :name, :contact_info
+
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+    @contact_info = options['contact_info']
+  end
+
+
+
+
+
+end
