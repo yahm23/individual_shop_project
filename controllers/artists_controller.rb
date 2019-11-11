80ede070b59f2c048ab9e30aaa10016f1ca77708
Yousef <yahmed1996a@gmail.com>
2019-11-11 09:44:22 +0000

    Artist page displaying the right format of showing all artists

--- controllers/artists_controller.rb
+++ controllers/artists_controller.rb
@@ -2,3 +2,10 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('../models/artist.rb')
 also_reload( '../models/*' )
+
+
+
+get '/artists' do
+   @artists = Artist.all
+  erb (:"artists/index")
+end
--- controllers/inventory_controller.rb
+++ controllers/inventory_controller.rb
@@ -4,3 +4,10 @@ require_relative('../models/record.rb')
 require_relative('../models/artist.rb')
 require_relative('../models/record_label.rb')
 also_reload( '../models/*' )
+
+
+
+get '/inventory' do
+  @records = Record.all
+  erb (:"inventory/index")
+end
